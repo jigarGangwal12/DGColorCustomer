5ee2496ba114d37f4e98b1584c75d4a32523d670
jigar <jigargangwal@jaychemical.com>
2022-12-27 17:43:50 +0530

    DS

--- src/app/secure/manufacturing/customer-dgcolor-report/customer-dgcolor-report.component.ts
+++ src/app/secure/manufacturing/customer-dgcolor-report/customer-dgcolor-report.component.ts
@@ -52,7 +52,7 @@ export class CustomerDGColorReportComponent implements OnInit {
 
   PredictionaData: boolean = false;
   showShadeMatchingData: boolean = false;
-  
+
   showOption1ShadeMatchingData: boolean = false;
   showOption2ShadeMatchingData: boolean = false;
   showOption3ShadeMatchingData: boolean = false;
@@ -69,6 +69,21 @@ export class CustomerDGColorReportComponent implements OnInit {
   rgbShadeMatching2: any;
   rgbShadeMatching3: any;
   rgbShadeMatching4: any;
+
+  moreThanOnePredctionOption: boolean = false;
+  moreThanOneMatchingOption: boolean = false;
+
+  remarkPopupVisible: boolean = false;
+  PredictionRemarkOp1: any;
+  PredictionRemarkOp2: any;
+  PredictionRemarkOp3: any;
+  PredictionRemarkOp4: any;
+  Remarks: any;
+  MatchingRemarkOp1: any;
+  MatchingRemarkOp2: any;
+  MatchingRemarkOp3: any;
+  MatchingRemarkOp4: any;
+
 
   constructor(private apiSevices: ApiServicesService, private route: ActivatedRoute) { }
 
@@ -92,7 +107,7 @@ export class CustomerDGColorReportComponent implements OnInit {
     };
     this.apiSevices.getAll(this.API_CONSTANTS.DgColorCustomerReport.CustomerReport.getCustomerDGColorReport, parmas).subscribe((data: any) => {
       this.customerDGColorReport = data.table;
-      if(data && data.table.length > 0) {
+      if (data && data.table.length > 0) {
         this.consigneeName = data.table[0].consigneeName;
         this.consigneeCode = data.table[0].consigneeCode;
         this.caseIdData = "";
@@ -108,6 +123,7 @@ export class CustomerDGColorReportComponent implements OnInit {
     this.loadingVisible = true;
     this.apiSevices.getAll(this.API_CONSTANTS.DgColorCustomerReport.CustomerReport.getDatabyShadeId, { shadeId: data.shadeid }).subscribe((data: any) => {
       if (data.table.length > 0) {
+        this.moreThanOnePredctionOption = false;
         this.PredictionaData = true;
         this.shadeMatchingData = false;
         this.recipeAllData = data.table;
@@ -117,16 +133,25 @@ export class CustomerDGColorReportComponent implements OnInit {
           this.showOption1RecipeData = true;
           this.receipeOptionData1 = data.table.filter((par: any) => par.predictionOption == 1);
           this.rgbCodeOption1 = this.receipeOptionData1[0].rgbCode;
-          this.Metameric1 = this.receipeOptionData1[0].primaryMetameric + "/" + this.receipeOptionData1[0].secondaryMetameric + "/" + this.receipeOptionData1[0].tertiaryMetameric;
+          // this.Metameric1 = this.receipeOptionData1[0].primaryMetameric + "/" + this.receipeOptionData1[0].secondaryMetameric + "/" + this.receipeOptionData1[0].tertiaryMetameric;
+          if (this.receipeOptionData1 && this.receipeOptionData1[0].matamerismByDataColor && this.receipeOptionData1[0].matamerismByDataColor != null && this.receipeOptionData1[0].matamerismByDataColor != undefined) {
+            this.Metameric1 = this.receipeOptionData1[0].matamerismByDataColor.slice(0, -6);
+          }
+          this.PredictionRemarkOp1 = this.receipeOptionData1[0].remarks;
         }
         else
           this.showOption1RecipeData = false;
 
         if (maxValueOftrail >= 2) {
+          this.moreThanOnePredctionOption = true;
           this.showOption2RecipeData = true;
           this.receipeOptionData2 = data.table.filter((par: any) => par.predictionOption == 2);
           this.rgbCodeOption2 = this.receipeOptionData2[0].rgbCode;
-          this.Metameric2 = this.receipeOptionData2[0].primaryMetameric + "/" + this.receipeOptionData2[0].secondaryMetameric + "/" + this.receipeOptionData2[0].tertiaryMetameric;
+          // this.Metameric2 = this.receipeOptionData2[0].primaryMetameric + "/" + this.receipeOptionData2[0].secondaryMetameric + "/" + this.receipeOptionData2[0].tertiaryMetameric;
+          if (this.receipeOptionData2 && this.receipeOptionData2[0].matamerismByDataColor && this.receipeOptionData2[0].matamerismByDataColor != null && this.receipeOptionData2[0].matamerismByDataColor != undefined) {
+            this.Metameric2 = this.receipeOptionData2[0].matamerismByDataColor.slice(0, -6);
+          }
+          this.PredictionRemarkOp2 = this.receipeOptionData2[0].remarks;
         }
         else
           this.showOption2RecipeData = false;
@@ -135,7 +160,11 @@ export class CustomerDGColorReportComponent implements OnInit {
           this.showOption3RecipeData = true;
           this.receipeOptionData3 = data.table.filter((par: any) => par.predictionOption == 3);
           this.rgbCodeOption3 = this.receipeOptionData3[0].rgbCode;
-          this.Metameric3 = this.receipeOptionData3[0].primaryMetameric + "/" + this.receipeOptionData3[0].secondaryMetameric + "/" + this.receipeOptionData3[0].tertiaryMetameric;
+          // this.Metameric3 = this.receipeOptionData3[0].primaryMetameric + "/" + this.receipeOptionData3[0].secondaryMetameric + "/" + this.receipeOptionData3[0].tertiaryMetameric;
+          if (this.receipeOptionData3 && this.receipeOptionData3[0].matamerismByDataColor && this.receipeOptionData3[0].matamerismByDataColor != null && this.receipeOptionData3[0].matamerismByDataColor != undefined) {
+            this.Metameric3 = this.receipeOptionData3[0].matamerismByDataColor.slice(0, -6);
+          }
+          this.PredictionRemarkOp3 = this.receipeOptionData3[0].remarks;
         }
         else
           this.showOption3RecipeData = false;
@@ -144,7 +173,11 @@ export class CustomerDGColorReportComponent implements OnInit {
           this.showOption4RecipeData = true;
           this.receipeOptionData4 = data.table.filter((par: any) => par.predictionOption == 4);
           this.rgbCodeOption4 = this.receipeOptionData4[0].rgbCode;
-          this.Metameric4 = this.receipeOptionData4[0].primaryMetameric + "/" + this.receipeOptionData4[0].secondaryMetameric + "/" + this.receipeOptionData4[0].tertiaryMetameric;
+          // this.Metameric4 = this.receipeOptionData4[0].primaryMetameric + "/" + this.receipeOptionData4[0].secondaryMetameric + "/" + this.receipeOptionData4[0].tertiaryMetameric;
+          if (this.receipeOptionData4 && this.receipeOptionData4[0].matamerismByDataColor && this.receipeOptionData4[0].matamerismByDataColor != null && this.receipeOptionData4[0].matamerismByDataColor != undefined) {
+            this.Metameric4 = this.receipeOptionData4[0].matamerismByDataColor.slice(0, -6);
+          }
+          this.PredictionRemarkOp4 = this.receipeOptionData4[0].remarks;
         }
         else
           this.showOption4RecipeData = false;
@@ -155,6 +188,7 @@ export class CustomerDGColorReportComponent implements OnInit {
       this.labPredictionParameter = data.table1;
 
       if (data.table3.length > 0) {
+        this.moreThanOneMatchingOption = false;
         this.showShadeMatchingData = true;
         this.PredictionaData = false;
         this.shadeMatchingAllData = data.table3;
@@ -164,14 +198,17 @@ export class CustomerDGColorReportComponent implements OnInit {
           this.showOption1ShadeMatchingData = true;
           this.shadeMatchingOptionData1 = data.table3.filter((par: any) => par.trail == 1);
           this.rgbShadeMatching1 = this.shadeMatchingOptionData1[0].rgbHexaCode;
+          this.MatchingRemarkOp1 = this.shadeMatchingOptionData1[0].remarks;
         }
         else
           this.showOption1ShadeMatchingData = false;
 
         if (maxValueOftrail >= 2) {
+          this.moreThanOneMatchingOption = true;
           this.showOption2ShadeMatchingData = true;
           this.shadeMatchingOptionData2 = data.table3.filter((par: any) => par.trail == 2);
           this.rgbShadeMatching2 = this.shadeMatchingOptionData2[0].rgbHexaCode;
+          this.MatchingRemarkOp2 = this.shadeMatchingOptionData2[0].remarks;
         }
         else
           this.showOption2ShadeMatchingData = false;
@@ -180,6 +217,7 @@ export class CustomerDGColorReportComponent implements OnInit {
           this.showOption3ShadeMatchingData = true;
           this.shadeMatchingOptionData3 = data.table3.filter((par: any) => par.trail == 3);
           this.rgbShadeMatching3 = this.shadeMatchingOptionData3[0].rgbHexaCode;
+          this.MatchingRemarkOp3 = this.shadeMatchingOptionData3[0].remarks;
         }
         else
           this.showOption3ShadeMatchingData = false;
@@ -188,6 +226,7 @@ export class CustomerDGColorReportComponent implements OnInit {
           this.showOption4ShadeMatchingData = true;
           this.shadeMatchingOptionData4 = data.table3.filter((par: any) => par.trail == 4);
           this.rgbShadeMatching4 = this.shadeMatchingOptionData4[0].rgbHexaCode;
+          this.MatchingRemarkOp4 = this.shadeMatchingOptionData4[0].remarks;
         }
         else
           this.showOption4ShadeMatchingData = false;
@@ -231,4 +270,15 @@ export class CustomerDGColorReportComponent implements OnInit {
   }
 
   PopupClose() { this.PopupVisible = false; }
+
+  remarkPopoUpOpen(remark: any) {
+    this.remarkPopupVisible = true;
+    if(remark){
+      this.Remarks = remark;
+    }
+  }
+  remarkPopupClose(event: any) {
+    this.Remarks = '';
+    this.remarkPopupVisible = false;
+  }
 }
